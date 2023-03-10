6c68e2a435afd1aa6e10a81e7c119c0373c7f7f4
Connor Andrews <76876626+connorrandrews@users.noreply.github.com>
2023-03-10 21:14:23 +0000

    Add files via upload

---
+++ rules.mk
@@ -0,0 +1,6 @@
+# This file intentionally left blank
+SPLIT_KEYBOARD = yes
+
+HAPTIC_ENABLE = yes
+HAPTIC_DRIVER += SOLENOID
+QK_HAPTIC_FEEDBACK_TOGGLE = keypress
---
+++ za84.h
@@ -0,0 +1,23 @@
+// Copyright 2023 connorrandrews (@connorrandrews)
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#pragma once
+#define USE_I2C
+#define MASTER_LEFT
+#define SPLIT_HAND_PIN 
+
+/*
+ * Feature disable options
+ *  These options are also useful to firmware size reduction.
+ */
+
+/* disable debug print */
+//#define NO_DEBUG
+
+/* disable print */
+//#define NO_PRINT
+
+/* disable action features */
+//#define NO_ACTION_LAYER
+//#define NO_ACTION_TAPPING
+//#define NO_ACTION_ONESHOT
